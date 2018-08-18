45abd3a51bb7ecfcab4a5c2b5c1904d8c91afda3
Matheus Icaro <matheusicaro2@hotmail.com>
2018-08-18 11:43:34 -0300

    initializing project.

---
+++ src/main/java/com/matheusicaro/course/fullstack/FullstackSpringbootApplication.java
@@ -0,0 +1,12 @@
+package com.matheusicaro.course.fullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackSpringbootApplication.class, args);
+	}
+}
